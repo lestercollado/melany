36cca0de7382542585db552406967d10660f605d
Lester Collado Rolo <lestercolladorolo1992@gmail.com>
2025-02-17 14:36:20 -0500

    set card explorer open default

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -10,7 +10,7 @@ import { staggerContainer } from '../utils/motion';
 import { exploreWorlds } from '../constants';
 
 const Explore = () => {
-  const [active, setActive] = useState('world-2');
+  const [active, setActive] = useState('world-1');
 
   return (
     <section className={`${styles.paddings}`} id="explore">
